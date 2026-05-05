60b13c69eb11e714ecd689d39a03bcb1d483adbd
12417434 <e12417434@student.tuwien.ac.at>
2026-05-05 19:31:39 +0200

    Repositories: Most of the entities now have a jpa repo, you can instert data by using them as shown in the DataInitializer class

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ReservationRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ReservationRepository.java
@@ -3,5 +3,5 @@ package at.ac.tuwien.sepr.groupphase.backend.repository;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
