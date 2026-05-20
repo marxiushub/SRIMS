265cd77e4b35cdbf7709ec9856734fe28db91022
AdrianSattlegger <sattlegger.adrian@hakspittal.at>
2026-05-20 17:36:44 +0200

    Updateing Attributes of CustomerProfile

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/user/CustomerProfile.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/user/CustomerProfile.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.sepr.groupphase.backend.entity.user;
 
+import at.ac.tuwien.sepr.groupphase.backend.entity.enums.SkillLevel;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
@@ -18,14 +19,30 @@ public class CustomerProfile {
     @Column(unique = false, length = 100)
     private String profileName;
 
+    @Column
+    private double height;
+
+    @Column
+    private double weight;
+
+    @Column
+    private double shoeSize;
+
+    @Column
+    private SkillLevel skillLevel;
+
     @ManyToOne
     @JoinColumn(name = "customer_id", nullable = false)
     private Customer customer;
 
     protected CustomerProfile() {}
 
-    public CustomerProfile(String profileName, Customer customer) {
+    public CustomerProfile(String profileName, double height, double weight, double shoeSize, SkillLevel skillLevel, Customer customer) {
         this.profileName = profileName;
+        this.height = height;
+        this.weight = weight;
+        this.shoeSize = shoeSize;
+        this.skillLevel = skillLevel;
         this.customer = customer;
     }
 
@@ -39,6 +56,38 @@ public class CustomerProfile {
 
     public void setProfileName(String profileName) {
         this.profileName = profileName;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public  double getShoeSize() {
+        return shoeSize;
+    }
+
+    public void setShoeSize(double shoeSize) {
+        this.shoeSize = shoeSize;
+    }
+
+    public  SkillLevel getSkillLevel() {
+        return skillLevel;
+    }
+
+    public void setSkillLevel(SkillLevel skillLevel) {
+        this.skillLevel = skillLevel;
     }
 
     public Customer getCustomer() {
