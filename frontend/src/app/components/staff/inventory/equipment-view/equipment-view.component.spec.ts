e6060f925e6c86811e0ca8be46e7a18b365c47e6
@12422744 <e12422744@student.tuwien.ac.at>
2026-05-21 14:03:51 +0200

    Added Frontend-Tests to Equipment-View

---
+++ frontend/src/app/components/staff/inventory/equipment-view/equipment-view.component.spec.ts
@@ -0,0 +1,64 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { EquipmentViewComponent } from './equipment-view.component';
+import { EquipmentService } from '../../../../services/equipment.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+
+//AI-assisted: Code generated with Google Gemini and adapted
+describe('EquipmentViewComponent', () => {
+  let component: EquipmentViewComponent;
+  let fixture: ComponentFixture<EquipmentViewComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [EquipmentViewComponent],
+      imports: [
+        RouterTestingModule,
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+        EquipmentService,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: () => '1'
+              }
+            }
+          }
+        }
+        // ----------------------------------------------------
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EquipmentViewComponent);
+    component = fixture.componentInstance;
+
+    const equipmentService = TestBed.inject(EquipmentService);
+    spyOn(equipmentService, 'getById').and.returnValue(of({
+      id: 1,
+      barcodeId: '12345',
+      price: 25.0,
+      model: 'Test Ski',
+      status: 'FREE',
+      targetSkillLevel: 'BEGINNER',
+      equipmentType: 'SKI',
+      length: 170
+    } as any));
+
+    fixture.detectChanges();
+  });
+
+  it('should create the view component', () => {
+    expect(component).toBeTruthy();
+  });
+});
