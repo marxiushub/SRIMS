e6d211eae8ce9f950042e6ff2f159f02ca25328a
Helene Landsmann <e11914664@student.tuwien.ac.at>
2026-05-25 23:34:23 +0200

    Add update logic for customer profile in service and endpoint

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/CustomerProfileServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/CustomerProfileServiceImpl.java
@@ -3,6 +3,7 @@ package at.ac.tuwien.sepr.groupphase.backend.service.impl;
 
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.customerprofile.CustomerProfileCreationDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.customerprofile.CustomerProfileDetailDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.customerprofile.CustomerProfileUpdateDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper.CustomerProfileMapper;
 import at.ac.tuwien.sepr.groupphase.backend.entity.user.Customer;
 import at.ac.tuwien.sepr.groupphase.backend.entity.user.CustomerProfile;
@@ -27,13 +28,17 @@ public class CustomerProfileServiceImpl implements CustomerProfileService {
     private final CustomerProfileRepository customerProfileRepository;
     private final CustomerRepository customerRepository;
     private final CustomerProfileMapper customerProfileMapper;
+    private final CustomerProfileValidator customerProfileValidator;
+
 
     public CustomerProfileServiceImpl(CustomerProfileRepository customerProfileRepository,
                                       CustomerRepository customerRepository,
-                                      CustomerProfileMapper mapper) {
+                                      CustomerProfileMapper mapper,
+                                      CustomerProfileValidator customerProfileValidator) {
         this.customerProfileRepository = customerProfileRepository;
         this.customerRepository = customerRepository;
         this.customerProfileMapper = mapper;
+        this.customerProfileValidator = customerProfileValidator;
     }
 
     @Override
@@ -66,6 +71,43 @@ public class CustomerProfileServiceImpl implements CustomerProfileService {
             throw new NotFoundException("Customer with ID " + customerId + " was not found.");
         }
         return customerProfileRepository.findByCustomerId(customerId).stream().map(customerProfileMapper::entityToDetailDto).toList();
+    }
+
+    @Override
+    public CustomerProfileDetailDto updateCustomerProfile(Long customerProfileId, CustomerProfileUpdateDto dto) {
+        LOGGER.trace("Updating customer profile for customer with id {}", customerProfileId);
+
+        if (customerProfileId == null) {
+            throw new IllegalArgumentException("Customer ID cannot be null.");
+        }
+
+        customerProfileValidator.validateUpdateDto(dto);
+
+        CustomerProfile profile = customerProfileRepository.findById(customerProfileId).orElseThrow(
+            () -> new NotFoundException("Customer profile with ID " + customerProfileId + " was not found."));
+
+        if (dto.getProfileName() != null) {
+            profile.setProfileName(dto.getProfileName());
+        }
+
+        if (dto.getHeight() != null) {
+            profile.setHeight(dto.getHeight());
+        }
+
+        if (dto.getWeight() != null) {
+            profile.setWeight(dto.getWeight());
+        }
+
+        if (dto.getShoeSize() != null) {
+            profile.setShoeSize(dto.getShoeSize());
+        }
+
+        if (dto.getSkillLevel() != null) {
+            profile.setSkillLevel(dto.getSkillLevel());
+        }
+
+        CustomerProfile savedProfile = customerProfileRepository.save(profile);
+        return customerProfileMapper.entityToDetailDto(savedProfile);
     }
 
     @Override
