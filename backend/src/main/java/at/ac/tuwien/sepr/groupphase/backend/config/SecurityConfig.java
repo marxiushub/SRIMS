103f5053b9e0b698026277cc3b1752e27722480c
Andre Zimmer Carral <e1171367@student.tuwien.ac.at>
2026-06-22 01:59:53 +0200

    Make a responsive layout for the customer profiles tab for smaller screen sizes

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/config/SecurityConfig.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:4200", "https://*.apps.student.inso-w.at")
+                .allowedOriginPatterns("http://localhost:4200", "http://192.168.*.*:4200", "https://*.apps.student.inso-w.at")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD");
         }
     }
