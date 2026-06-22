e995f4ca649cb1584f7cca2efe8a687f450c4d6a
Andre Zimmer Carral <e1171367@student.tuwien.ac.at>
2026-06-22 14:08:11 +0200

    Fix navbar closing for buttons with stopPropagation and add navbar service tests

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import {AuthService} from '../../services/auth.service';
 import {TranslateService} from "@ngx-translate/core";
 import {AppComponent} from "../../app.component";
 import { Collapse } from 'bootstrap';
+import {NavbarService} from "../../services/navbar.service";
 
 @Component({
     selector: 'app-header',
@@ -16,9 +17,11 @@ export class HeaderComponent implements OnInit {
     public authService: AuthService,
     public app: AppComponent,
     private translate: TranslateService,
+    private navbarService: NavbarService,
     private elementRef: ElementRef) { }
 
   ngOnInit() {
+    this.navbarService.closeNavbar$.subscribe(() => this.closeNavbar());
   }
 
   get isEnglish(): boolean {
@@ -47,6 +50,17 @@ export class HeaderComponent implements OnInit {
           bsCollapse.hide();
         }
       }
+    }
+  }
+
+  toggleNavbar(): void {
+    const navbarEl = document.getElementById('navbarSupportedContent');
+    if (navbarEl) {
+      let bsCollapse = Collapse.getInstance(navbarEl);
+      if (!bsCollapse) {
+        bsCollapse = new Collapse(navbarEl, { toggle: false });
+      }
+      bsCollapse.toggle();
     }
   }
 
