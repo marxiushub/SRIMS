b30c525c4c7bccc3c13b61395fade1c881c0657a
Andre Zimmer Carral <e1171367@student.tuwien.ac.at>
2026-06-25 02:03:28 +0200

    Fix overview frontend test by adding missing imports

--- frontend/src/app/components/staff/overview/overview.component.spec.ts
+++ frontend/src/app/components/staff/overview/overview.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { OverviewComponent } from './overview.component';
+import {EMPTY} from 'rxjs';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {OverviewComponent} from './overview.component';
+import {EquipmentService} from '../../../services/equipment.service';
+import {TranslateModule} from '@ngx-translate/core';
 
 describe('OverviewComponent', () => {
   let component: OverviewComponent;
@@ -8,9 +10,12 @@ describe('OverviewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [OverviewComponent]
-    })
-    .compileComponents();
+      declarations: [OverviewComponent],
+      imports: [TranslateModule.forRoot()],
+      providers: [
+        {provide: EquipmentService, useValue: {getStatusOverview: () => EMPTY}}
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(OverviewComponent);
     component = fixture.componentInstance;
